8345c59b856f598293d5e5702d843e92ab90b070
Wanderley Souza <wandss@gmail.com>
2019-03-20 18:33:25 -0300

    Create gnuicorn_start.sh

---
+++ gnuicorn_start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+NAME="DJANGOPROJECTNAME"
+DJANGODIR=/path/to/django/project/root
+SOCKFILE=/path/to/django/project/root/DJANGOPROJECTNAME.sock
+USER=
+GROUP=
+NUM_WORKERS=4
+DJANGO_SETTINGS_MODULE=DJANGOPROJECTNAME.settings.prd
+DJANGO_WSGI_MODULE=DJANGOPROJECTNAME.wsgi
+SECRET_KEY='whereverSecretKey'
+TIMEOUT=5000
+
+echo "Starting $NAME as `whowami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+echo $DJANGODIR
+echo $DJANGO_WSGI_MODULE
+source /path/to/virtualenv/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export SECRET_KEY=$SECRET_KEY
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Creates "run" directory if it doesn't exists yet
+# RUNDIR=$(dirname $SOCKFILE)
+
+#Start Django Unicorn
+exec /path/to/virtualenv/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user $USER \
+  --bind=unix:$SOCKFILE \
+  --timeout $TIMEOUT \
