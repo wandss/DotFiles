fa9bffa3ef6de681f79947dfb72ddf72089621d5
Wanderley Souza <wandss@gmail.com>
2019-07-01 22:22:09 -0300

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+  productionSourceMap: false,  
+  outputDir: path.resolve(__dirname, process.env.VUE_APP_BUILD_PATH),
+  indexPath: path.resolve(__dirname, process.env.VUE_APP_INDEX_PATH),  
+  publicPath: process.env.VUE_APP_PUBLIC_PATH
+  devServer: {
+    proxy: 'http://0.0.0.0:8001
+  }
+}  
